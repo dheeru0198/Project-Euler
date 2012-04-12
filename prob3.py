c2a8cfb23b2a80e07efea8df8e0d4be18786b5fe
Dheeraj Kumar Ketireddy <dheeru0198@gmail.com>
2012-04-12 16:24:13 +0530

    Changed few lines

--- prob3.py
+++ prob3.py
@@ -2,22 +2,22 @@
 # Problem - 3
 # Find the largest prime factor of a composite number.
 
-c = int(raw_input("Enter any Composite number : "))
-i = 2
-j = 1
-cnt = 0
+from math import sqrt
 
-while i < c :
+c = int(raw_input("Enter any Composite number : "))
+i = 3
+
+def is_prime(n) :
+    for j in range(2, int(sqrt(n)) + 1) :
+        if (n % j == 0) :
+            return 0
+    return 1
+
+while (i < c) :
     if  c % i == 0 :
         x = c / i
-        while j < x :
-            if x % j == 0 :
-                cnt = cnt + 1
-            j = j + 1
-        j = 1
-        if (cnt == 1) :
+        if is_prime(x) :
             lpf = x
             break
-        cnt = 0
-    i = i + 1
+    i += 1
 print lpf
